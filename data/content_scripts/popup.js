ef8d30fcb522f758b5814f6505d2d0f8d34e7dc5
hitesh96db <hitesh96db@gmail.com>
2015-06-01 22:20:34 +0400

    Open link on clicking icons

--- data/content_scripts/popup.js
+++ data/content_scripts/popup.js
@@ -2,10 +2,10 @@ self.port.on("extensionMode", function(message) {
   if (typeof message === "string") {
     extMode = window.document.getElementById("extensionMode");
     if (message === "active") {
-      extMode.className = "label label-success";
+      extMode.className = "label label-success event-toggleMode";
       extMode.innerHTML = "On";
     } else {
-      extMode.className = "label label-danger";
+      extMode.className = "label label-danger event-toggleMode";
       extMode.innerHTML = "Off";
     }
   }
@@ -13,7 +13,15 @@ self.port.on("extensionMode", function(message) {
 
 window.addEventListener("click", function(event) {
   var target = event.target;
-  if (target.nodeName === "A") {
-    self.port.emit("hyperlink", target.id);
+  var className = target.className;
+  if (target.nodeName === "A" || target.nodeName === "SPAN") {
+    if (typeof className !== "undefined") {
+      var searchFor = "event-"; 
+      var length = searchFor.length;
+      var idx = className.indexOf(searchFor);
+      if (idx !== -1) {
+        self.port.emit("hyperlink", className.substring(idx + length, className.length));
+      }
+    }
   }
 }, false);
